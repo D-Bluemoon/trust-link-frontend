cb9b5dd8aa9a4f8574d96a2b3382981d4a7e171a
CAESAR MCMXCVII <rubelu2017@gmail.com>
2026-06-02 11:54:31 +0100

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { NetworkProvider } from "@/components/providers/NetworkProvider";
 import { WalletProvider } from "@/components/providers/WalletProvider";
 import { NotificationProvider } from "@/components/providers/NotificationProvider";
 import I18nProvider from "@/components/providers/I18nProvider";
@@ -39,28 +40,30 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <TestnetBanner />
-        <ServiceWorkerProvider />
-        <a
-          href="#main-content"
-          className="sr-only focus:not-sr-only focus:absolute focus:z-[100] focus:p-4 focus:bg-white focus:text-black focus:font-semibold"
-        >
-          Skip to content
-        </a>
-        <WalletProvider>
-          <NotificationProvider>
-            <I18nProvider>
-              <Navbar />
-              {/* pb-20 on mobile gives room for the fixed BottomNav; md:pb-0 removes it on desktop */}
-              <main id="main-content" tabIndex={-1} className="flex flex-1 flex-col pb-20 md:pb-0 outline-none">
-                {children}
-              </main>
-              <Footer />
-              <BottomNav />
-              <Toaster richColors position="top-right" />
-            </I18nProvider>
-          </NotificationProvider>
-        </WalletProvider>
+        <NetworkProvider>
+          <TestnetBanner />
+          <ServiceWorkerProvider />
+          <a
+            href="#main-content"
+            className="sr-only focus:not-sr-only focus:absolute focus:z-[100] focus:p-4 focus:bg-white focus:text-black focus:font-semibold"
+          >
+            Skip to content
+          </a>
+          <WalletProvider>
+            <NotificationProvider>
+              <I18nProvider>
+                <Navbar />
+                {/* pb-20 on mobile gives room for the fixed BottomNav; md:pb-0 removes it on desktop */}
+                <main id="main-content" tabIndex={-1} className="flex flex-1 flex-col pb-20 md:pb-0 outline-none">
+                  {children}
+                </main>
+                <Footer />
+                <BottomNav />
+                <Toaster richColors position="top-right" />
+              </I18nProvider>
+            </NotificationProvider>
+          </WalletProvider>
+        </NetworkProvider>
         <Toaster position="bottom-right" />
       </body>
     </html>
